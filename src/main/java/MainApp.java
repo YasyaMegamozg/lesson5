09de7ea4eac09cdf06907b86d1acfa9d63abb6cb
Yasmin Bashikova <yasya121104@gmail.com>
2025-09-15 22:47:01 +0500

    второе задание

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -2,14 +2,26 @@ import java.util.Arrays;
 
 public class MainApp {
     public static void main(String[] args) {
-        stringAdd(7,"hello World");
+//        stringAdd(7,"hello World");
+        int[] arr = {1, 6, 3, 8, 9, 2, 4, 7};
+        printArr(arr);
+
     }
 
-    public static void stringAdd(int a, String b) {
-        for (int i = 0; i < a; i++) {
-            System.out.println(b);
-            
+    //    public static void stringAdd(int a, String b) {
+//        for (int i = 0; i < a; i++) {
+//            System.out.println(b);
+//        }
+//    }
+    public static void printArr(int[] arr) {
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>5) {
+                sum+=arr[i];
+            }
         }
-
+        System.out.println("сумма элементов: " + sum);
     }
+
+
 }
