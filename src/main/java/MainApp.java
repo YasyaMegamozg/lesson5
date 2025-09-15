30a96296c7eb09d19e19e910fdc6f4eeca583f34
Yasmin Bashikova <yasya121104@gmail.com>
2025-09-15 22:54:57 +0500

    третье задание

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 public class MainApp {
     public static void main(String[] args) {
 //        stringAdd(7,"hello World");
-        int[] arr = {1, 6, 3, 8, 9, 2, 4, 7};
-        printArr(arr);
-
+//        int[] arr = {1, 6, 3, 8, 9, 2, 4, 7};
+//        printArr(arr);
+        int []arr ={1,3,6,8,};
+        numberAndArr(5, arr);
     }
 
     //    public static void stringAdd(int a, String b) {
@@ -13,15 +14,22 @@ public class MainApp {
 //            System.out.println(b);
 //        }
 //    }
-    public static void printArr(int[] arr) {
-        int sum=0;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>5) {
-                sum+=arr[i];
-            }
-        }
-        System.out.println("сумма элементов: " + sum);
-    }
 
+//    public static void printArr(int[] arr) {
+//        int sum=0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if(arr[i]>5) {
+//                sum+=arr[i];
+//            }
+//        }
+//        System.out.println("сумма элементов: " + sum);
+//    }
+
+    public static void numberAndArr(int n, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=n;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 
 }
