bd68995c598b46454dec23e430c106ae6b96e734
Yasmin Bashikova <yasya121104@gmail.com>
2025-09-15 22:36:19 +0500

    первое задание

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -2,6 +2,14 @@ import java.util.Arrays;
 
 public class MainApp {
     public static void main(String[] args) {
-        
+        stringAdd(7,"hello World");
+    }
+
+    public static void stringAdd(int a, String b) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(b);
+            
+        }
+
     }
 }
